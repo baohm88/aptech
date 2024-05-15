96d1c095028cd07b074a0e9039763f6f77720bf0
Bao Ha <baohm88@gmail.com>
2024-05-15 10:18:02 +0700

    Added find min, max of array entered by the user

---
+++ arrays/array_minmax.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    // Get num of arr elements from user
+    int arr_size, min, max;
+    printf("Enter the number of array elements: ");
+    scanf("%d", &arr_size);
+
+    // Loop through array and assign a value to each array element
+    int arr[arr_size];
+    for (int i = 0; i < arr_size; i++)
+    {
+        printf("Enter the number of element #%d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    // Loop through arr, compare and re-assign min/max value
+    min = arr[0];
+    max = arr[0];
+    for (int i = 1; i < arr_size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        
+        if (arr[i] > max )
+        {
+            max = arr[i];
+        }
+    }
+    printf("Min of the array: %d\nMax of the array: %d\n", min, max);
+
+    return 0;
+}
